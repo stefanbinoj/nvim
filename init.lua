87c5724a17427ad9c34668fda68c271da37bf305
stefan binoj <stefan.binoj.007@gmail.com>
2025-08-17 08:32:06 +0530

    chore: break down code

--- init.lua
+++ init.lua
@@ -23,38 +23,8 @@ vim.opt.rtp:prepend(lazypath)
 
 local opts = {}
 
-local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-     dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-  {"nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
-    end
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    }
-
-  }
-}
-
-require("lazy").setup(plugins, opts)
+require("lazy").setup("plugins")
 
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
