95cd5636a7081acbe41262f79976c7c3383cab6e
stefan binoj <stefan.binoj.007@gmail.com>
2025-08-17 09:14:40 +0530

    chore: splitted code

--- init.lua
+++ init.lua
@@ -1,13 +1,4 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader= " "
-vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal float<CR>')
-
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
     "git",
@@ -18,17 +9,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     lazypath,
   })
 end
-
 vim.opt.rtp:prepend(lazypath)
 
-local opts = {}
-
-
+require("vim-options")
 require("lazy").setup("plugins")
 
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
