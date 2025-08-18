406dd269c3626250ea35cfe844adfda269af78a5
stefan binoj <stefan.binoj.007@gmail.com>
2025-08-18 16:24:12 +0530

    added vim conf

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,6 +1,14 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader= " "
+-- Leader key
+vim.g.mapleader = " "
 
+-- UI settings
+vim.opt.scrolloff = 8
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Indentation
+vim.opt.expandtab = true         -- Use spaces instead of tabs
+vim.opt.tabstop = 4              -- Number of spaces a tab counts for
+vim.opt.softtabstop = 4          -- Number of spaces a <Tab> feels like
+vim.opt.shiftwidth = 4           -- Indentation amount
+vim.opt.smartindent = true       -- Smart indenting on new lines
