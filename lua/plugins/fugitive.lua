4b62086f77c266024a90f66abd38a197f916a32f
stefan binoj <stefan.binoj.007@gmail.com>
2025-09-04 14:16:16 +0530

    chore: added fugitive

---
+++ lua/plugins/fugitive.lua
@@ -0,0 +1,35 @@
+return {
+    "tpope/vim-fugitive",
+    config = function()
+        -- Global shortcut: Git status
+        vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Git status" })
+
+        -- Autocmd to set buffer-local keymaps for Fugitive buffers
+        vim.api.nvim_create_autocmd("BufWinEnter", {
+            group = vim.api.nvim_create_augroup("MyFugitive", { clear = true }),
+            pattern = "*",
+            callback = function()
+                if vim.bo.ft ~= "fugitive" then
+                    return
+                end
+
+                local opts = { buffer = vim.api.nvim_get_current_buf(), noremap = true, silent = true }
+
+                vim.keymap.set("n", "<leader>p", function()
+                    vim.cmd.Git("push")
+                end, vim.tbl_extend("force", opts, { desc = "Git push" }))
+
+                vim.keymap.set("n", "<leader>P", function()
+                    vim.cmd.Git({ "pull", "--rebase" })
+                end, vim.tbl_extend("force", opts, { desc = "Git pull --rebase" }))
+
+                vim.keymap.set(
+                    "n",
+                    "<leader>t",
+                    ":Git push -u origin ",
+                    vim.tbl_extend("force", opts, { desc = "Git push -u" })
+                )
+            end,
+        })
+    end,
+}
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,44 +1,43 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "ts_ls" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = false,
+        opts = {
+            auto_install = true,
+        },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "ts_ls" },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+            local lspconfig = require("lspconfig")
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-			})
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.ts_ls.setup({
+                capabilities = capabilities,
+            })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-		end,
-	},
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+            vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, {})
+        end,
+    },
 }
