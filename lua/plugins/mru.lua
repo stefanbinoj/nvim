c797a12654c67756c6688b6502d5e9bfddee67bb
stefan binoj <stefan.binoj.007@gmail.com>
2025-11-04 17:11:30 +0530

    feat: telescope fr

--- lua/plugins/mru.lua
+++ lua/plugins/mru.lua
@@ -2,15 +2,16 @@ return {
 	"wsdjeg/mru.nvim",
 	dependencies = { "nvim-telescope/telescope.nvim" },
 	keys = {
-		{ "<leader>mru", "<cmd>Telescope mru<cr>", desc = "Most Recently Used Files" },
+		{ "<leader>m", "<cmd>Telescope mru<cr>", desc = "Most Recently Used Files" },
 	},
 	config = function()
 		require("mru").setup({
 			enable_cache = true,
 			mru_cache_file = vim.fn.stdpath("data") .. "/nvim-mru.json",
-			events = { "BufEnter", "BufWritePost" }, -- keep MRU updated
+			-- events = { "BufEnter", "BufWritePost" }, -- keep MRU updated
 			ignore_path_regexs = { "/.git/" },
 			enable_logger = false, -- set true only if you also install wsdjeg/logger.nvim
+			sort_by = "lastenter",
 		})
 	end,
 }
